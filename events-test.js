f75ce576093bb05efd8e968b2828b05d1e3ae0b1
Dnyaneshwar Shetkar <dshetkar0101@gmail.com>
2024-08-09 13:46:27 +0530

    Date 09-08-2024

---
+++ events-test.js
@@ -0,0 +1,31 @@
+function clickEventHandler(e){
+    e.target.style.color = "red";
+}
+
+function mouseOverEventHandler(e){
+    alert("You mouse pointer is over me");
+}
+
+function mouseOutHandler(e){
+    alert("Your mouse pointer is outside now");
+}
+
+function keydownEventHandler(e){
+    console.log(e);
+}
+
+document.getElementById("username").addEventListener("change", (ev)=>{
+   // console.log(ev);
+})
+document.getElementById("username").addEventListener("change", (ev)=>{
+   // console.log(ev);
+})
+document.getElementById("username").addEventListener("change", (ev)=>{
+    //console.log(ev);
+})
+document.getElementById("username").addEventListener("change", (ev)=>{
+    //console.log(ev);
+})
+document.getElementById("username").addEventListener("change", (ev)=>{
+    //console.log(ev);
+})
